4322b28ada4fc47348f854561bc1b8b5c423e12e
YM Liao <ymliao@beingfreebutterfact.com>
2026-05-11 23:11:59 +0800

    fix: set --base-href=/butterfact-demo/ for GitHub Pages asset paths

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4272985756" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2273754450" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
